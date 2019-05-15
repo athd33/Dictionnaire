43da6234655dd7d627e186db6605eef47dfeb610
athd33 <antoinecours@gmx.fr>
2019-05-15 08:00:57 +0200

    DictionnaireOrdonne fin

--- dictionnaire.py
+++ dictionnaire.py
@@ -1,40 +1,38 @@
-# Exercice du dictionnaire ordonné - correction
-
-
-import time
-
-
-
 class DictionnaireOrdonne:
-    """Création de la classe DictionnaireOrdonne"""
+    """Dictionnaire ordonné"""
     
-    def __init__(self, dicto={}, **donnees):                                    # instanciation
-        """Constructeur de l'objet, instanciation de la classe."""
+    def __init__(self, base={}, **donnees):             ###### 1er POINT #####
+        """
+        Méthode d'instanciation du Dictionnaire:
+        base={} créé un dictionnaire par défaut, si rien n'est renseigné en paramètre,
+        (**donnees) entre des paramètres sous forme de dictionnaire avec ** """
         
-        self.cles = []                                                         # création d'une liste vide pour les clés
-        self.valeurs = []                                                      # création d'une liste pour les valeurs
+        self.cles = [] # Liste contenant nos clés
+        self.valeurs = [] # Liste contenant les valeurs correspondant à nos clés
         
-                                                                               # On vérifie que 'dicto' est de type dict()
-        if type(dicto) not in (dict, DictionnaireOrdonne):
-            raise TypeError("Attention, il doit s'agir d'un dictionnaire")
+        # On vérifie que 'base' est un dictionnaire exploitable
+        if type(base) not in (dict, DictionnaireOrdonne):
+            raise TypeError( \
+                "le type attendu est un dictionnaire (usuel ou ordonne)")
         
-                                                                                # On récupère les données de 'dicto'
-        for cle in dicto:
-            self[cle] = dicto[cle]
+        # On récupère les données de 'base'
+        for cle in base:
+            self[cle] = base[cle]
         
-                                                                                # On récupère les données de 'donnees'
+        # On récupère les données de 'donnees'
         for cle in donnees:
             self[cle] = donnees[cle]
     
 
-
-    def __repr__(self):
-        """Représentation  de l'objet avec la méthode spéciale  'repr'"""
+    def __repr__(self):                                 ###### 2eme POINT  ######
+        """Représentation de notre objet. C'est cette chaîne qui sera affichée
+        quand on saisit directement le dictionnaire dans l'interpréteur, ou en
+        utilisant la fonction 'repr'"""
         
-        chaine = "{"            # initialisation de la chaine 
+        chaine = "{"
         premier_passage = True
         for cle, valeur in self.items():
-            if not premier_passage: # condition permettant de placer la séparation
+            if not premier_passage:
                 chaine += ", " # On ajoute la virgule comme séparateur
             else:
                 premier_passage = False
@@ -43,22 +41,23 @@ class DictionnaireOrdonne:
         return chaine
     
     def __str__(self):
-        """méthode spéciale pour pouvoir utiliser print"""        
+        """Méthode spéciale pour pouvoir utiliser 'print' """
         return repr(self)
     
     def __len__(self):
-        """Pour utiliser len, et connaitre la taille de l'élément"""
+        """Renvoie la taille du dictionnaire"""
         return len(self.cles)
     
     def __contains__(self, cle):
-        """Pour vérifier si la clé est dans la l'objet"""
+        """Permet de rechercher la clé dans le dictionnaire avec 'in'"""
         return cle in self.cles
     
     def __getitem__(self, cle):
-        """Renvoie la valeur correspondant à la clé si elle existe, lèveune exception KeyError sinon"""
+        """Renvoie la valeur correspondant à la clé si elle existe, lève
+        une exception KeyError sinon"""
         
         if cle not in self.cles:
-            raise KeyError( f"La clé {cle} ne se trouve pas dans le dictionnaire")
+            raise KeyError(f"La clé {cle} ne se trouve pas dans le dictionnaire")
         else:
             indice = self.cles.index(cle)
             return self.valeurs[indice]
@@ -78,7 +77,9 @@ class DictionnaireOrdonne:
     def __delitem__(self, cle):
         """Méthode appelée quand on souhaite supprimer une clé"""
         if cle not in self.cles:
-            raise KeyError( f"La clé {cle} ne se trouve pas dans le dictionnaire")
+            raise KeyError( \
+                "La clé {0} ne se trouve pas dans le dictionnaire".format( \
+                cle))
         else:
             indice = self.cles.index(cle)
             del self.cles[indice]
@@ -89,10 +90,12 @@ class DictionnaireOrdonne:
         return iter(self.cles)
     
     def __add__(self, autre_objet):
-        """Retourne deux dictionnaires, self en premier et 'l'ajouté' en second """
+        """Dictionnaires mis bout à bout (d'abord self puis autre_objet)"""
         
         if type(autre_objet) is not type(self):
-            raise TypeError(f"Impossible de concaténer {type.self} et {type(autre_objet)}")
+            raise TypeError( \
+                "Impossible de concaténer {0} et {1}".format( \
+                type(self), type(autre_objet)))
         else:
             nouveau = DictionnaireOrdonne()
             
@@ -106,21 +109,22 @@ class DictionnaireOrdonne:
             return nouveau
     
     def items(self):
-        """Renvoie un générateur contenant les couples (cle, valeur)"""
+        """Renvoie les couples (cle, valeur)"""
         for i, cle in enumerate(self.cles):
             valeur = self.valeurs[i]
             yield (cle, valeur)
     
     def keys(self):
-        """Cette méthode renvoie la liste des clés"""
+        """Renvoie les clés"""
         return list(self.cles)
     
     def values(self):
-        """Cette méthode renvoie la liste des valeurs"""
+        """Renvoir la liste des valeurs"""
         return list(self.valeurs)
     
     def reverse(self):
-        """Tri inversé"""
+        """Inversion du dictionnaire"""
+        # Création de deux listes pour ordonner le nouveau dictionnaire
         cles = []
         valeurs = []
         for cle, valeur in self.items():
@@ -132,54 +136,65 @@ class DictionnaireOrdonne:
         self.valeurs = valeurs
     
     def sort(self):
-        """Tri par clé"""
-        # Tri des clés par ordre alphabétique
+        """Sort pour trier le dictionnaire par clés"""
+        # On trie les clés
         cles_triees = sorted(self.cles)
-
         # On crée une liste de valeurs, encore vide
         valeurs = []
-
         # On parcourt ensuite la liste des clés triées
         for cle in cles_triees:
             valeur = self[cle]
             valeurs.append(valeur)
-            
         # Enfin, on met à jour notre liste de clés et de valeurs
         self.cles = cles_triees
         self.valeurs = valeurs
 
 
 def myprint(mess):
-    time.sleep(1)
-    print(f"\n{mess}\n")
+    print(f"{mess}\n")
 
 
 
 
 
-        #################### TESTS DU DICTIONNAIRE ############################
+####### TESTS ####################
 
-myprint("### Création d'un dictionnaire vide :")
-fruits = DictionnaireOrdonne()                                   # Instanciation d'un dictionnaire vide
-myprint(f"Le dictionnaire fruits a été créé : {type(fruits)}")
+####### 1er POINT #######
+myprint("instanciation de fruits avec la méthode __init__() :")
 
-fruits["pommes"] = 52
-fruits["poires"] = 34
-fruits["prunes"] = 128
-fruits["melons"] = 15
+fruits = DictionnaireOrdonne()
 
-myprint(fruits)                             
+myprint("Ajouts de clés / valeurs dans le dictionnaire : ")
 
-fruits["bananes"] = 21                                          # Ajout d'un élément clé/valeur dans le dictionnaire
+fruits["pommes"] = 44
+fruits["poires"] = 21
+fruits["prunes"] = 221
+fruits["melons"] = 93
 
-myprint(fruits)
+print(f"\nAffichage du dictionnaire avec print : {fruits}:")
 
-print(fruits["melons"])                                         # les indices sont couplés, melons retourne 15
+# modifications possibles
+
+fruits["pommes"] = "00, plus de pommes"
+myprint(f"\nModification de la valeur de la clé 'pommes' {fruits}:")
 
 
-del fruits["melons"]                                            
-myprint("Suppression des clés 'melons/ et sa valeur' avec del, possible grace a la méthode delitem :")   # la méthode spéciale delitem permet de supprimer clé/valeur grace à del
-myprint(fruits)
+                            # in permis par la méthode contains
+if "bananes" in fruits:
+    print("Il y a des bananes")
+else:
+    print("Pas de bananes")
 
-print(fruits)
-print("items ci dessous :")
+print(f"\nLa longueur du dictionnaire est de {len(fruits)} clés")
+
+
+                    # parcours du dictionnaire par clé
+for cle in fruits:
+    print(cle)
+
+                                                            # renvoie les clés
+print(f"\n Affichage des clés avec keys() : {fruits.keys()}")
+
+                                                            # renvoie les valeurs
+print(f"\n Affichage des valeurs avec values() : {fruits.values()}")
+
